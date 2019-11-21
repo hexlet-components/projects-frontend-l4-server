7342e83930b4746f9f3b32113fee9beb62fd342f
Aleksandr Dargeev <dargeev@gmail.com>
2019-11-21 19:14:14 +0700

    fix fastify.listen host for heroku

--- server/bin/slack.js
+++ server/bin/slack.js
@@ -1,4 +1,6 @@
 import getApp from '..';
 
+const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1';
 const port = process.env.PORT || 4000;
-getApp().listen(port, () => console.log(`port: ${port}`));
+
+getApp().listen(port, host, () => console.log(`listening on: ${host}: ${port}`));
