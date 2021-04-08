079856a2be7fbf575b99a91245fd8614a00861e4
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-04-08 21:32:21 +0300

    fix build url

--- __tests__/messages.test.js
+++ __tests__/messages.test.js
@@ -1,9 +1,8 @@
 // @ts-check
 
-import path from 'path';
 import buildApp from '../server/index.js';
 
-const buildUrl = (url) => path.join('/api/v1/', url);
+const buildUrl = (url) => `/api/v1/${url}`;
 
 test('get /channels/:id/messages', async () => {
   const state = {
